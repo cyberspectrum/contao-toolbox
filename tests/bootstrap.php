f559048849c7060548de7f995c4ba03760cc67b5
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-07-09 13:10:29 +0200

    Add build job layout.

---
+++ tests/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This toolbox provides easy ways to generate .xlf (XLIFF) files from Contao language files, push them to transifex
+ * and pull translations from transifex and convert them back to Contao language files.
+ *
+ * @package      cyberspectrum/contao-toolbox
+ * @author       Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright    CyberSpectrum
+ * @license      LGPL-3.0+.
+ * @filesource
+ */
+
+error_reporting(E_ALL);
+
+function includeIfExists($file)
+{
+    return file_exists($file) ? include $file : false;
+}
+
+if ((!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php'))
+    && (!$loader = includeIfExists(__DIR__ . '/../../../autoload.php'))
+) {
+    echo 'You must set up the project dependencies, run the following commands:' . PHP_EOL .
+        'curl -sS https://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL;
+    exit(1);
+}
+
+$loader->add('CyberSpectrum', __DIR__);
+
+require __DIR__ . '/CyberSpectrum/ContaoToolbox/Test/TestCase.php';
