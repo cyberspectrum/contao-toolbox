15a0aa3ff6518f84c6447954c80380b1f542b9b7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-07-10 00:23:05 +0200

    Add empty class ResourceStats to empty file.

---
+++ src/CyberSpectrum/Transifex/ResourceStats.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This toolbox provides easy ways to generate .xlf (XLIFF) files from Contao language files, push them to transifex
+ * and pull translations from transifex and convert them back to Contao language files.
+ *
+ * @package      cyberspectrum/contao-toolbox
+ * @author       Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright    CyberSpectrum
+ * @license      LGPL-3.0+.
+ * @filesource
+ */
+
+namespace CyberSpectrum\Transifex;
+
+/**
+ * This class represents resource statistics.
+ */
+class ResourceStats
+{
+}
