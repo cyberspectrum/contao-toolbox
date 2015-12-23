0c1b9710e07ae1c77e444875fb71550e5bf1fc41
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-12-23 08:37:55 +0100

    Fix issue #20 - autoload_files was not added

--- src/CyberSpectrum/Compiler.php
+++ src/CyberSpectrum/Compiler.php
@@ -90,6 +90,7 @@ class Compiler
         $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/autoload_namespaces.php'));
         $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/autoload_classmap.php'));
         $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/autoload_real.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/autoload_files.php'));
         if (file_exists(__DIR__ . '/../../vendor/composer/include_paths.php')) {
             $this->addFile($phar, new \SplFileInfo(__DIR__ . '/../../vendor/composer/include_paths.php'));
         }
