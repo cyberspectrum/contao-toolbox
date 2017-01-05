05735022571065cf720803d8238adc844b9538ee
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-01-05 22:18:05 +0100

    Fix #19  transifex link now links to signup

--- src/CyberSpectrum/Translation/Contao/ContaoFile.php
+++ src/CyberSpectrum/Translation/Contao/ContaoFile.php
@@ -143,7 +143,7 @@ class ContaoFile extends AbstractFile
  * Translations are managed using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/signup/
  * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
