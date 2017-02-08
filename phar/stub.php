085072cf771272b14803daa0e47985ce3954bc2f
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-02-08 18:42:16 +0100

    Add copyright to stub

--- phar/stub.php
+++ phar/stub.php
@@ -1,10 +1,21 @@
 #!/usr/bin/env php
 <?php
-/*
- * This file is part of ctb.
+
+/**
+ * This file is part of cyberspectrum/contao-toolbox.
  *
- * For the full copyright and license information, please view
- * the license that is located at the bottom of this file.
+ * (c) 2013-2017 CyberSpectrum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    cyberspectrum/contao-toolbox.
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2013-2017 CyberSpectrum.
+ * @license    https://github.com/cyberspectrum/contao-toolbox/blob/master/LICENSE LGPL-3.0
+ * @filesource
  */
 
 if (version_compare(phpversion(), '@@CTB_MIN_PHP_VERSION@@', '<=')) {
