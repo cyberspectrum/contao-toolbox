3fe3e662ec6f0e58e3000f44fc2fe564a2bcd90d
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-02-08 19:23:56 +0100

    Fix default file header in translations

--- src/Translation/Contao/ContaoFile.php
+++ src/Translation/Contao/ContaoFile.php
@@ -186,10 +186,10 @@ class ContaoFile extends AbstractFile
  * Translations are managed using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
  * @link https://www.transifex.com/signup/
  * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
- *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
  * last-updated: $$lastchanged$$
  */
